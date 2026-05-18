1313418932765024fc42f19437b1ff25c59600b6
omer1798 <230316032@ogr.cbu.edu.tr>
2026-05-18 21:59:03 +0300

    Add Main class to initialize and display Employee

---
+++ Java101/maas_hesaplayici/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("omer",3000,60,2025);
+        System.out.println(e1.toString());
+    }
+}
